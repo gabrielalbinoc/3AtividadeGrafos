9356f2df989594cf3adc98e641d919e8b6784947
gabrielalbinoc <102620439+gabrielalbinoc@users.noreply.github.com>
2022-09-23 12:21:51 -0300

    Update MatrizAdj.java

--- src/main/java/MatrizAdj.java
+++ src/main/java/MatrizAdj.java
@@ -4,74 +4,64 @@ import java.util.Queue;
 
 
 public class MatrizAdj {
-    LeitorTXT lerTXT = new LeitorTXT("C:/Users/Gabriel/IdeaProjects/Atividade1Grafos/src/main/java/pequenoG.txt");
+    LeitorTXT lerTXT = new LeitorTXT("src/main/java/pequenoG.txt");
     ArrayList<String> linhas = lerTXT.read();
     String [] ante = new String[Integer.parseInt(linhas.get(0))];
+    int s = 0;
+    int[][] matAdj = new int[Integer.parseInt(linhas.get(0))][Integer.parseInt(linhas.get(1))];
     public static void main(String[] args) {
-        LeitorTXT lerTXT = new LeitorTXT("C:/Users/Gabriel/IdeaProjects/Atividade1Grafos/src/main/java/pequenoG.txt");
+        MatrizAdj run = new MatrizAdj();
+        LeitorTXT lerTXT = new LeitorTXT("src/main/java/pequenoG.txt");
         ArrayList<String> linhas = lerTXT.read();
-        int[][] matAdj = new int[Integer.parseInt(linhas.get(0))][Integer.parseInt(linhas.get(1))];
-        for (int i = 0; i < matAdj.length; i++) {
+        for (int i = 0; i < run.matAdj.length; i++) {
             String[] valores = linhas.get(i + 2).split(" ");
-            matAdj[Integer.parseInt(valores[0])][Integer.parseInt(valores[1])] = 1;
-            matAdj[Integer.parseInt(valores[1])][Integer.parseInt(valores[0])] = 1;
+            run.matAdj[Integer.parseInt(valores[0])][Integer.parseInt(valores[1])] = 1;
+            run.matAdj[Integer.parseInt(valores[1])][Integer.parseInt(valores[0])] = 1;
+        }
         System.out.println("Matriz Adjacência");
-            for (int j = 0; j < matAdj[0].length; j++) {
+        for (int i = 0; i < run.matAdj.length; i++) {
+            for (int j = 0; j < run.matAdj[0].length; j++) {
 
-                System.out.print(matAdj[i][j] + "  ");
+                System.out.print(run.matAdj[i][j] + "  ");
             }
             System.out.println();
         }
         System.out.println();
-        System.out.println("========== Printing BFS ==========");
-        bfs(matAdj,5);
+        System.out.println("========== Imprimindo BFS ==========");
+        run.bfs(run.matAdj,run.s);
     }
 
-    void bfs(int[][] matAdj, int inicio){
-        String [] cor = new String[matAdj.length];
-        double [] d = new double[matAdj.length];
-        double inf = Double.POSITIVE_INFINITY;
+    double inf = Double.POSITIVE_INFINITY;
+    double [] d = new double[matAdj.length];
+    String[] cor = new String[matAdj.length];
+    
+    void bfs (int [][]matAdj, int s) {
+        int u, v;
         Queue<Integer> fila = new LinkedList<>();
-        for (int i = 0; i < matAdj.length; i++) {
-            for (int j = 0; j < matAdj[0].length; j++) {
-                if(matAdj[i][j]==1 || matAdj[j][i]==1 && matAdj[i][j] != inicio) {
-                    cor[j] = "branco";
-                    d[j] = inf;
-                    ante[j] = null;
-                }
-                if (i == inicio) {
-                    cor[inicio] = "cinza";
-                    d[inicio] = 0;
-                    fila.add(inicio);
-                }
-                while (!fila.isEmpty()){
-                    fila.poll();
+        for (u = 0; u < matAdj.length; u++) {
+            cor[u] = "BRANCO";
+            d[u] = inf;
+            ante[u] = null;
+            cor[s] = "CINZA";
+            d[s] = 0;
+            fila.add(s);
+        }
+        for (u = 0; u < matAdj.length; u++) {
+            while (!fila.isEmpty()) {
+                u = fila.poll();
+            }
+            for (v = 0; v < matAdj[0].length; v++) {
+                if (matAdj[u][v] == 1) {
+                    if (cor[v] == "BRANCO") {
+                        cor[v] = "CINZA";
+                        d[v] = d[u] + 1;
+                        ante[v] = String.valueOf(u);
+                        fila.add(v);
+                    }
                 }
             }
+            cor[u] = "PRETO";
         }
-        for (int i = 0; i < matAdj.length; i++) {
-            for (int j = 0; j < matAdj[0].length; j++) {
-                if (cor[j] == "branco") {
-                    cor[j] = "cinza";
-                    d[j] = d[i] + 1;
-                    ante[j] = String.valueOf(j - 1);
-                    fila.add(j);
-                }
-                cor[j] = "preto";
-            }
-        }
-        printBfs(6);
-    }
-    void printBfs(int vertice) {
-        for (int i = 0; i < ante.length; i++) {
-            if (vertice == i) {
-                System.out.println(i);
-            } else if (ante[vertice] == null) {
-                System.out.println("Não há caminho");
-            } else {
-                printBfs(Integer.parseInt(ante[vertice]));
-                System.out.println(vertice);
-            }
-        }
+         printCaminho(6);
     }
 }
